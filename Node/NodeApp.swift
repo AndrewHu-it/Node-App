f44e5e96269aadf9a62ef6d0d7824afb7921fedd
andrew <ahurlbut999@gmail.com>
2025-03-01 17:13:34 -0800

    Initial Commit

---
+++ Node/NodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeApp.swift
+//  Node
+//
+//  Created by Andrew Hurlbut on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct NodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NodeTests/NodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NodeTests.swift
+//  NodeTests
+//
+//  Created by Andrew Hurlbut on 3/1/25.
+//
+
+import Testing
+@testable import Node
+
+struct NodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
